1e756949ef6b2305300383cde20e6a10a58ce36f
ThomasRavetto <travetto18@gmail.com>
2023-09-23 09:36:06 +0200

    update

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+const fs = require('fs')
 const path = require('path');
+const https = require('https');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3500;
 
 const app = express();
 
+app.use(helmet())
+
+app.get('/secret', (req, res) => {
+    res.send('Your secret value is 33!')
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
     console.log(`Listening on port:`, PORT)
 })
