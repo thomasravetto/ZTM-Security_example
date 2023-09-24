0d863c97f8dcfe01a0b135802d8a8d4c9c79c448
ThomasRavetto <travetto18@gmail.com>
2023-09-24 11:59:15 +0200

    final commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session')
 
 require('dotenv').config()
 
@@ -13,6 +14,8 @@ const PORT = 3500;
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 }
 
 const AUTH_OPTIONS = {
@@ -28,13 +31,31 @@ const verifyCallback = (accessToken, refreshToken, profile, done) => {
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
+// Save session to cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+// Read session from cookie
+passport.deserializeUser((id, done) => {
+    done(null, id);
+})
+
 const app = express();
 
 app.use(helmet());
+
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000, // One 24 hours day
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}))
+
 app.use(passport.initialize());
+app.use(passport.session());
 
 const checkLoggedIn = (req, res, next) => {
-    const isLoggedIn = true;
+    const isLoggedIn = req.isAuthenticated() && req.user;
     if (!isLoggedIn) {
         return res.status(401).json({
             error: 'You must log in!'
@@ -52,14 +73,17 @@ app.get('/auth/google/callback',
     passport.authenticate('google', {
         failureRedirect: '/failure',
         successRedirect: '/',
-        session: false,
+        session: true,
     }),
     (req, res) => {
         console.log('Google called us back!')
     }
 );
 
-app.get('/auth/logout', (req, res) => {});
+app.get('/auth/logout', (req, res) => {
+    req.logout(); // Removes req.user and clears session
+    return res.redirect('/');
+});
 
 
 // Passing a middleware to this function to check if the user is logged in
